aa6dd808ec09daba3b0ec15681521d2840dadc7b
Satya1929 <spswain2003.org@gmail.com>
2024-10-05 19:47:53 +0530

    for deployement in streamlit i added the requirements

--- Main.py
+++ Main.py
@@ -3,18 +3,20 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Generative AI App",
+    page_title="V-Buddy App",
     page_icon="🤖",
 )
 
-st.write("# Welcome to the Generative AI Q&A App! 🤖")
+st.write("# Welcome to the Generative AI QnA App! 🤖")
 st.sidebar.success("Select a page from the sidebar to navigate.")
 
 st.markdown("""
     This application allows users to ask questions to a Generative AI model 
-    based on uploaded PDFs. Navigate to the appropriate page to begin!
+    based on uploaded context PDFs. Navigate to the appropriate page to begin!
     
     ### Pages Available:
     - 📂 **Backend Page**: Developers can upload PDFs here.
-    - 🤖 **AI Q&A Page**: Users can interact with the Generative AI using the uploaded PDFs.
+    - 🤖 **AI Q&A Page**: Users can interact with the Generative AI with context of uploaded files.
+    - 📝 **Feedback Page**: Users can provide feedback for improvement.
+    - 🎉 **Credits Page**: Anyone can see the contributors of the V-Buddy
 """)
